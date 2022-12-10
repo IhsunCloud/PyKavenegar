6056b527b9a7975a9d214e86830355bdc8392604
ihsunmozhdeh <41645055+ihsunmozhdeh@users.noreply.github.com>
2022-12-11 01:48:07 +0330

    Add generate_otp function.

---
+++ generate_otp.py
@@ -0,0 +1,17 @@
+import math
+import random
+
+
+def generate_otp(password_len=6):
+	"""
+	  Reusable function to generate an OTP password.
+ 	
+  	ihsun@tuta.io
+	"""
+	passwd_len = password_len
+	DIGITS = '0123456789'
+	OTP	= ''
+	
+	for number in range(passwd_len) :
+		OTP += DIGITS[math.floor(random.random() * 10)]
+	return OTP
